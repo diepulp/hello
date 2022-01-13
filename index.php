ecc5db87f9f4cb08d34016f8c7643260fdf96f93
Vladimir Ivanov <Ivanov.Vladimir@student.greenriver.edu>
2022-01-13 17:37:49 -0500

    Home page view added

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     //define a default route
     $f3 -> route('GET /', function(){
         echo "<h1>Hello world</h1>";
+        $view = new Template();
+        echo $view -> render('views/home.html');
 });
 
     //Run fat-free
