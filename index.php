de8299bb33c193c2cfe400aa3bf1361f1458f9e0
Vladimir Ivanov <Ivanov.Vladimir@student.greenriver.edu>
2022-01-13 17:32:41 -0500

    controller added

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+
+    require_once("vendor/autoload.php");
+
+    //create an instance of the Base Class
+    $f3 = Base::instance();
+
+    //define a default route
+    $f3 -> route('GET /', function(){
+        echo "<h1>Hello world</h1>";
+});
+
+    //Run fat-free
+    $f3 -> run();
